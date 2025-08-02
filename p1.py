70aeee08e295aea8d52e64adbe4717a868272463
sanjay gamit <sanjugamit@gmail.com>
2025-08-02 11:46:37 +0530

    file modified

--- p1.py
+++ p1.py
@@ -1,2 +1,11 @@
 import streamlit as st 
-print('hello world')
+# print('hello world')
+
+st.title("Hi I am Streamlit Web App")
+st.subheader("Hi I am your subheader!")
+st.header("Hi I am your header!")
+st.text("Hi I am your text!")
+
+
+
+
